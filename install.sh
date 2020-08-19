5ca6b796188a06999132764e06acb72f0e028ae9
Matt Camilli <matt@camilli.dev>
2020-08-19 10:22:33 -0400

    updated to include tmux stuff

--- install.sh
+++ install.sh
@@ -11,5 +11,12 @@ ln -s ${BASEDIR}/.zshrc ~/.zshrc
 # git
 ln -s ${BASEDIR}/.gitconfig ~/.gitconfig
 
+# Tmux
+ln -s ${BASEDIR}/.tmuxp ~/.tmuxp
+ln -s ${BASEDIR}/.tmux.conf ~/.tmux.conf
+
+
+
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+pip3 install tmuxp black isort flake8

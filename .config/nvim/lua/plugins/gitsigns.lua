c3c2f508a4ca6f31b87e110950abacf3f9ee90f7
Matt Camilli <matt@camilli.dev>
2022-08-08 08:31:12 -0400

    fix blame highlighting

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -3,7 +3,8 @@ require('gitsigns').setup {
     current_line_blame_opts = {
         virt_text = true,
         virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-        delay = 1000,
-        ignore_whitespace = false,
+        delay = 500,
+        ignore_whitespace = true,
     },
 }
+vim.cmd([[highlight link GitSignsCurrentLineBlame Visual]])

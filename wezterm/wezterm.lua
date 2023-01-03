81fa862c89ce7293854e67b78267097860bbe75c
Matt Camilli <git@camilli.dev>
2023-01-03 13:08:25 -0500

    re enable keybinds

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm'
 return {
   font = wezterm.font 'Hack',
-  font_size = 11.0, 
+  font_size = 11.0,
   color_scheme = 'Dracula (Official)',
   tab_bar_at_bottom = false,
   use_fancy_tab_bar = false,
@@ -11,7 +11,7 @@ return {
   automatically_reload_config = true,
   check_for_updates = true,
   default_cursor_style = "BlinkingBlock",
-  disable_default_key_bindings = true,
+  disable_default_key_bindings = false,
   hide_tab_bar_if_only_one_tab = true,
   enable_tab_bar = true,
 }

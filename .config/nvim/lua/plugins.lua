98fae53934fb599b634df380de60b52ee119adac
Matt Camilli <git@camilli.dev>
2023-01-18 12:10:05 -0500

    we are lazy now

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,80 +1,87 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 --
+--
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
 local fn = vim.fn
 local cmd = vim.cmd
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  cmd [[packadd packer.nvim]]
-end
-
-cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-  augroup end
-]])
-
-return require('packer').startup(function(use)
-    use {
+require("lazy").setup({
+    {
+        'dracula/vim',
+        name='dracula',
+        lazy=false,
+        priority=1000,
+        config = function()
+            cmd([[colorscheme dracula]])
+        end,
+    },
+    {
         'kyazdani42/nvim-tree.lua',
-        requires = {'kyazdani42/nvim-web-devicons', },
+        dependencies = {'kyazdani42/nvim-web-devicons', },
         tag = 'nightly',
         config = function() require('plugins.nvim-tree') end
-    }
-    use {'dracula/vim', as = 'dracula' }
-    use 'flazz/vim-colorschemes' use 'ConradIrwin/vim-bracketed-paste'
-    use {
+    },
+    'flazz/vim-colorschemes',
+    'ConradIrwin/vim-bracketed-paste',
+    {
         'nvim-treesitter/nvim-treesitter',
         run = function() require('nvim-treesitter.install').update(
             { with_sync = true , prefer_git = true})
         end,
         config = function() require('plugins.nvim-treesitter') end
-    }
-    use 'ryanoasis/vim-devicons'
-    use({
+    },
+    'ryanoasis/vim-devicons',
+    {
         "kylechui/nvim-surround",
         config = function() require("nvim-surround").setup({}) end
-    })
-    use {
+    },
+    {
         'numToStr/Comment.nvim',
         config = function() require('Comment').setup() end
-    }
-    use "williamboman/mason.nvim"
-    use 'williamboman/mason-lspconfig.nvim'
-    use 'neovim/nvim-lspconfig'
-    use 'jose-elias-alvarez/null-ls.nvim'
-    use {
+    },
+    "williamboman/mason.nvim",
+    'williamboman/mason-lspconfig.nvim',
+    'neovim/nvim-lspconfig',
+    'jose-elias-alvarez/null-ls.nvim',
+    {
         'hrsh7th/nvim-cmp',
-        requires = {
+        dependencies = {
             'hrsh7th/cmp-nvim-lsp',
             'hrsh7th/cmp-buffer',
             'hrsh7th/cmp-path',
             'hrsh7th/cmp-cmdline',
             'hrsh7th/cmp-nvim-lsp-signature-help'
         }
-    }
-    use {
+    },
+    {
         'nvim-lualine/lualine.nvim',
         config = function() require('plugins.lualine') end
-    }
-    use {
+    },
+    {
         'lewis6991/gitsigns.nvim',
-        tag = 'main',
-        requires = {'nvim-lua/plenary.nvim'},
+        branch = 'main',
+        dependencies = {'nvim-lua/plenary.nvim'},
         config = function() require('plugins.gitsigns') end
-    }
-    use {
+    },
+    {
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.0',
+        tag = '0.1.1',
+        dependencies = {{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" }, 'nvim-lua/plenary.nvim'},
         config = function() require('plugins.telescope') end
-    }
-    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-    use({
+    },
+    {
         "iamcco/markdown-preview.nvim",
         run = function() vim.fn["mkdp#util#install"]() end,
-    })
-    if packer_bootstrap then
-        require('packer').sync()
-    end
-end)
+    }
+})

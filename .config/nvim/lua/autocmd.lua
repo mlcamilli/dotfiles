38b944b7c679ca1ef54e739bea45d30b649d98de
Matt Camilli <git@camilli.dev>
2023-10-28 09:48:17 -0400

    added ruff support, auto push branches

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -1,4 +1,5 @@
 local cmd = vim.cmd
+local api = vim.api
 
 
 cmd([[
@@ -9,3 +10,12 @@ autocmd BufReadPost quickfix nnoremap <buffer> <CR> <CR>
 
 -- Delte trailing white space
 cmd([[autocmd BufWritePre * :%s/\s\+$//e]])
+
+
+api.nvim_create_autocmd("BufWritePre", {
+    callback = function()
+        -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+        vim.lsp.buf.format({async = False})
+        vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true })
+    end,
+})
--- .config/nvim/lua/lsp/nvim-lsp.lua
+++ .config/nvim/lua/lsp/nvim-lsp.lua
@@ -1,4 +1,3 @@
-
 vim.o.updatetime = 250
 vim.api.nvim_create_autocmd("CursorHold", {
   -- buffer = bufnr,
@@ -21,7 +20,7 @@ local on_attach = function(client, bufnr)
   client.server_capabilities.semanticTokensProvider = nil
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
   vim.keymap.set('n', 'gK', vim.lsp.buf.hover, bufopts)
@@ -34,79 +33,87 @@ local icons = _G.icons
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local servers = {
-    pyright = {
-        on_init = function(client)
-            local path = vim.fn.getcwd()
-            -- Project specific overrides can go here, % to escape any characters
-            if string.find(path, 'rhythm%-app') then
-                client.config.settings.python.analysis.diagnosticSeverityOverrides = {reportMissingImports = "none"}
-                client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-            end
-            return true
-        end,
-        settings = {
-            python = {
-                analysis = {
-                    diagnosticSeverityOverrides = {
-                    }
-                }
-            }
+  pyright = {
+    on_init = function(client)
+      local path = vim.fn.getcwd()
+      -- Project specific overrides can go here, % to escape any characters
+      if string.find(path, 'rhythm%-app') then
+        client.config.settings.python.analysis.diagnosticSeverityOverrides = { reportMissingImports = "none" }
+        client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+      end
+      return true
+    end,
+    capabilities = (function()
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities.textDocument.publishDiagnostics.tagSupport.valueSet = { 2 }
+      return capabilities
+    end)(),
+    disableOrganizeImports = true,
+    settings = {
+      python = {
+        analysis = {
+          diagnosticSeverityOverrides = {
+          }
         }
-    },
-    tsserver = {},
-    rust_analyzer = {},
-    lua_ls =  {
-        settings = {
-            Lua = {
-                diagnostics = {
-                    globals = {'vim'}
-                },
-                library = {
-                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                },
-            }
-        }
-    },
-    astro = {},
-    cmake = {},
-    tailwindcss = {},
-    terraformls = {}
+      }
+    }
+  },
+  ruff_lsp = {
+  },
+  tsserver = {},
+  rust_analyzer = {},
+  lua_ls = {
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = { 'vim' }
+        },
+        library = {
+          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+        },
+      }
+    }
+  },
+  astro = {},
+  cmake = {},
+  tailwindcss = {},
+  terraformls = {}
 }
 
 local lsp_config = require('lspconfig')
 for name, user_opts in pairs(servers) do
-	local lsp_opts = {
-		on_attach = on_attach,
-		capabilities = capabilities,
-	}
+  local lsp_opts = {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
 
-	lsp_opts = vim.tbl_deep_extend("force", lsp_opts, user_opts)
-	lsp_config[name].setup(lsp_opts)
+  lsp_opts = vim.tbl_deep_extend("force", lsp_opts, user_opts)
+  lsp_config[name].setup(lsp_opts)
 end
 
 vim.fn.sign_define('DiagnosticSignError',
-    { text = icons.Error, texthl = 'DiagnosticSignError' }
+  { text = icons.Error, texthl = 'DiagnosticSignError' }
 )
 vim.fn.sign_define('DiagnosticSignWarn',
-    { text = icons.Warning, texthl = 'DiagnosticSignWarn' }
+  { text = icons.Warning, texthl = 'DiagnosticSignWarn' }
 )
 vim.fn.sign_define('DiagnosticSignInfo',
-    { text = icons.Info, texthl = 'DiagnosticSignInfo' }
+  { text = icons.Info, texthl = 'DiagnosticSignInfo' }
 )
 vim.fn.sign_define('DiagnosticSignHint',
-    { text = icons.Hint, texthl = 'DiagnosticSignHint' }
+  { text = icons.Hint, texthl = 'DiagnosticSignHint' }
 )
 vim.diagnostic.config({
-    virtual_text = false,
-    update_in_insert = true,
-    underline = true,
-    severity_sort = true,
-    float = {
-        focusable = false,
-        style = "minimal",
-        border = "rounded",
-        source = "always",
-        header = "",
-        prefix = "",
-    },
+  virtual_text = false,
+  update_in_insert = true,
+  underline = true,
+  severity_sort = true,
+  float = {
+    focusable = false,
+    style = "minimal",
+    border = "rounded",
+    source = "always",
+    header = "",
+    prefix = "",
+  },
 })

4881bf222fed372499f64a501c724f708ffacece
Matt Camilli <git@camilli.dev>
2023-11-28 10:41:46 -0500

    don't tag telescope

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -91,7 +91,6 @@ require("lazy").setup(
     },
     {
       'nvim-telescope/telescope.nvim',
-      tag = '0.1.1',
       dependencies = { { "nvim-telescope/telescope-fzf-native.nvim", build = "make" }, 'nvim-lua/plenary.nvim' },
       config = function() require('plugins.telescope') end
     },

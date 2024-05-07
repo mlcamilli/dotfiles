3f3e4c4629bac8e0aa27c3bb6b856cb9aa8fca7e
Matt Camilli <git@camilli.dev>
2024-05-07 12:31:32 -0400

    configs

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -7,7 +7,7 @@ return {
   end,
   event = "VeryLazy",
   config = function()
-    require('nvim-treesitter.config').setup({
+    require('nvim-treesitter.configs').setup({
       -- One of "all", "maintained" (parsers with maintainers), or a list of languages
       ensure_installed = { "lua", "python", "rust", "javascript", "dockerfile", "go", "json", "make", "toml", "yaml",
         "typescript", "markdown", "css", "html", "bash", "tsx" },

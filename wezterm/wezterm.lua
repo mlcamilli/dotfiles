7e40c205a8e3b29f8adbf043c1da64205752d391
Matt Camilli <git@camilli.dev>
2024-05-08 11:55:33 -0400

    updated wezterm settings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,12 +1,12 @@
 local wezterm = require 'wezterm'
 return {
-  font = wezterm.font 'Hack',
+  font = wezterm.font 'Hack Nerd Font',
   font_size = 11.0,
   color_scheme = 'Dracula (Official)',
   tab_bar_at_bottom = false,
   use_fancy_tab_bar = false,
   window_decorations = "RESIZE",
-  default_prog = {'wsl', '~', '-d', 'Ubuntu-20.04'},
+  default_prog = { 'wsl', '~', '-d', 'Ubuntu-20.04' },
   window_background_opacity = .99,
   automatically_reload_config = true,
   check_for_updates = true,
@@ -14,4 +14,11 @@ return {
   disable_default_key_bindings = false,
   hide_tab_bar_if_only_one_tab = true,
   enable_tab_bar = true,
+  keys = {
+    {
+      key = 'm',
+      mods = 'CTRL',
+      action = wezterm.action.DisableDefaultAssignment,
+    },
+  },
 }

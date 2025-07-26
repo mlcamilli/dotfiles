733fd6d33d83ff642caac7abaea9e63dd7d934b2
Matt Camilli <matt@camilli.dev>
2025-07-26 08:13:05 -0400

    Mini pick (#9)
    * shift to snacks

--- .config/nvim/lua/core/colors.lua
+++ .config/nvim/lua/core/colors.lua
@@ -3,3 +3,9 @@ vim.cmd([[
     hi! link FloatBorder DraculaWhite
     hi! link Keywords DraculaCyan
 ]])
+
+-- Make hidden files use normal text color in Snacks picker/explorer
+vim.api.nvim_set_hl(0, "SnacksPickerPathHidden", { link = "Normal" })
+
+-- Make directories use the same highlight as files in Snacks picker
+vim.api.nvim_set_hl(0, "SnacksPickerDir", { link = "SnacksPickerFile" })
--- .config/nvim/lua/core/init.lua
+++ .config/nvim/lua/core/init.lua
@@ -1,7 +1,6 @@
 require('core.lazy')
 require('core.options')
 require('core.filetype')
-require('core.statuscolumn')
 require('core.colors')
 require('core.autocmd')
 require('core.keymaps')
